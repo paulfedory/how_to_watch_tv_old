72e63289dab34df1ddd6804edd02dde087eaf435
Paul Fedory <paul.fedory@gmail.com>
2016-12-18 14:52:23 -0500

    Add HTML5 Up template 'lens'

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,8 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+//
+//
+//import jQuery from "./jquery.min"
+//import "./skel.min"
+import "./main"
