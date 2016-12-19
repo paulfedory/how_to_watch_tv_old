e7c98b93f1464ae8c72c40f94ad881311795320e
Paul Fedory <paul.fedory@gmail.com>
2016-12-19 16:45:38 -0500

    Add heroku deploy info

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,18 @@ use Mix.Config
 # which you typically run after static files are built.
 config :how_to_watch_tv, HowToWatchTv.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  # url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "polar-garden-44069.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+config :how_to_watch_tv, HowToWatchTv.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -62,4 +72,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
