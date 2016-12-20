7aaf6152932cf1d0041f6aa327767ad31f13a3c5
Paul Fedory <paul.fedory@gmail.com>
2016-12-19 22:38:18 -0500

    Revert "Force SSL, I think? I can't remember how this works"
    This reverts commit e7e62b729d355abfeb7cfcf29737b9bc6f551145.

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,6 @@ config :how_to_watch_tv, HowToWatchTv.Endpoint,
   # url: [host: "example.com", port: 80],
   url: [scheme: "http", host: "howtowatch.tv", port: 443],
   # force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  force_ssl: [hsts: true],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
