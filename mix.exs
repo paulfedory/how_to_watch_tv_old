848d1af837e79cdb94844588dc2dc7b7489a5d64
Paul Fedory <paul.fedory@gmail.com>
2016-12-21 17:18:52 -0500

    Start ExTV application on phoenix startup

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule HowToWatchTv.Mixfile do
   def application do
     [mod: {HowToWatchTv, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :extv]]
   end
 
   # Specifies which paths to compile per environment.
